5c3d9011962b6b91fd254dc71203bdd833ba33a0
John Serrano <andrey_201015@hotmail.com>
2019-08-27 15:43:55 -0500

    Implementando la capa de servicios

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const path = require('path')
 const productsRouter = require('./routes/products')
 const productsApiRouter = require('./routes/api/products')
+const bodyParser = require('body-parser')
 
 const port = process.env.port || 8000
 
@@ -15,6 +16,8 @@ app.set('view engine', 'pug')
 
 app.use('/products', productsRouter)
 app.use('/api/products', productsApiRouter)
+
+app.use(bodyParser.json())
 
 app.listen(port, function () {
   console.log(`Listening http://localhost:${port}`)
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,18 @@
 const epxress = require('express')
 const router = epxress.Router()
 
-const productMocks = require('../utils/mocks/products')
+const ProductsService = require('../services/products')
 
-router.get('/', (req, res) => {
-  res.render('products', { productMocks })
+const productService = new ProductsService()
+
+router.get('/', async function (req, res, next) {
+  const { tags } = req.query
+  try {
+    const products = await productService.getProducts({ tags })
+    res.render('products', { products })
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
