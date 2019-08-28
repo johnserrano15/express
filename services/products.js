f45c9de5f4dbe5de0849896937aeb75b64c0674e
John Serrano <andrey_201015@hotmail.com>
2019-08-28 15:46:20 -0500

    Conectando nuestros servicios con mongo DB

--- services/products.js
+++ services/products.js
@@ -1,26 +1,32 @@
 const productsMocks = require('../utils/mocks/products')
+const MongoLib = require('../lib/mongo')
 
 class ProductsService {
-  // constructor () {
-  // }
-
-  getProducts ({ tags }) {
-    return Promise.resolve(productsMocks)
+  constructor() {
+    this.collection = 'products'
+    this.mongoDB = new MongoLib()
   }
 
-  getProduct ({ productId }) {
+  async getProducts({ tags }) {
+    const query = tags && { tags: { $in: tags } }
+    const products = await this.mongoDB.getAll(this.collection, query)
+
+    return products || []
+  }
+
+  getProduct({ productId }) {
     return Promise.resolve(productsMocks[0])
   }
 
-  createProduct ({ product }) {
+  createProduct({ product }) {
     return Promise.resolve(productsMocks[0])
   }
 
-  updateProduct ({ productId, product }) {
+  updateProduct({ productId, product }) {
     return Promise.resolve(productsMocks[0])
   }
 
-  deleteProduct ({ productId }) {
+  deleteProduct({ productId }) {
     return Promise.resolve(productsMocks[0])
   }
 }
