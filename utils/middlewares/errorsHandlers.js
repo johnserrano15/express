488a578b5d63e78974d4526754c6bb5ae5e14a4b
John Serrano <andrey_201015@hotmail.com>
2019-09-04 11:24:06 -0500

    Implementando Sentry

--- utils/middlewares/errorsHandlers.js
+++ utils/middlewares/errorsHandlers.js
@@ -1,6 +1,10 @@
+const Sentry = require('@sentry/node')
 const { config } = require('../../config')
 
+Sentry.init({ dsn: `https://${config.sentryDns}@sentry.io/${config.sentryId}` })
+
 function logErrors (err, req, res, next) {
+  Sentry.captureException(err)
   console.log(err.stack)
   next(err)
 }
