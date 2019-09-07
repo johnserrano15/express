4526887d106fd9f531f46696caca131199292555
John Serrano <andrey_201015@hotmail.com>
2019-09-07 10:52:06 -0500

    Creando tests para nuestros servicios

--- services/products.js
+++ services/products.js
@@ -1,47 +1,47 @@
-const productsMocks = require('../utils/mocks/products')
+// const productsMocks = require('../utils/mocks/products')
 const MongoLib = require('../lib/mongo')
 
 class ProductsService {
-  constructor() {
+  constructor () {
     this.collection = 'products'
     this.mongoDB = new MongoLib()
   }
 
-  async getProducts({ tags }) {
+  async getProducts ({ tags }) {
     const query = tags && { tags: { $in: tags } }
     const products = await this.mongoDB.getAll(this.collection, query)
 
     return products || []
   }
 
-  async getProduct({ productId }) {
-    const product = await this.mongoDB.get(this.collection, productId);
-    return product || {};
+  async getProduct ({ productId }) {
+    const product = await this.mongoDB.get(this.collection, productId)
+    return product || {}
   }
 
-  async createProduct({ product }) {
-    const createProductId = await this.mongoDB.create(this.collection, product);
+  async createProduct ({ product }) {
+    const createProductId = await this.mongoDB.create(this.collection, product)
 
-    return createProductId;
+    return createProductId
   }
 
-  async updateProduct({ productId, product }) {
+  async updateProduct ({ productId, product }) {
     const updateProductId = await this.mongoDB.update(
       this.collection,
       productId,
       product
-    );
+    )
 
-    return updateProductId;
+    return updateProductId
   }
 
-  async deleteProduct({ productId }) {
+  async deleteProduct ({ productId }) {
     const deletedProductId = await this.mongoDB.delete(
       this.collection,
       productId
-    );
+    )
 
-    return deletedProductId;
+    return deletedProductId
   }
 }
 
