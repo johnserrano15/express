9a429901dbe4e16f7b99e3be8b1ac329a063a9c3
John Serrano <andrey_201015@hotmail.com>
2019-09-09 10:25:13 -0500

    npm script

--- routes/views/products.js
+++ routes/views/products.js
@@ -2,6 +2,7 @@ const epxress = require('express')
 const router = epxress.Router()
 
 const ProductsService = require('../../services/products')
+const { config } = require('../../config')
 
 const productService = new ProductsService()
 
@@ -9,7 +10,7 @@ router.get('/', async function (req, res, next) {
   const { tags } = req.query
   try {
     const products = await productService.getProducts({ tags })
-    res.render('products', { products })
+    res.render('products', { products, dev: config.dev })
   } catch (err) {
     next(err)
   }
